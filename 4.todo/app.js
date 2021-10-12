9b4e4cb106ee43ed9f34825376c78992d50ee43d
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-10-11 22:19:12 -0400

    Agregando los modelos de tarea
    - se agrego la class tarea y tareas
    - ahora se puede trabajar con esas clases

--- 4.todo/app.js
+++ 4.todo/app.js
@@ -1,17 +1,22 @@
 require('colors')
 const { inquireMenu, pausa } = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
 
 
-
-console.clear()
 
 const main = async () => {
 
     let opt = ''
 
     do {
-        opt = await inquireMenu()
-        console.log({ opt })
+        // opt = await inquireMenu()
+        // console.log({ opt })
+
+
+        const tareas = new Tareas()
+        const tarea = new Tarea('Comprar comida')
+        console.log(tarea)
 
         await pausa()
 
