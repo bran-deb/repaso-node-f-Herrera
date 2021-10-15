db3c1ac1197c6af948ab56e8eff8a68c33210d7c
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-10-14 23:46:57 -0400

    Se agrego la funcion de leer datos de la db
    - se agrego t testeo la funcion de leerdb
    - ahora podemos obtener los datos de la db

--- 4.todo/db/guardarArchivo.js
+++ 4.todo/db/guardarArchivo.js
@@ -1,15 +1,31 @@
 const fs = require('fs')
 
 
+//en archivo especificamos el directorio y el tipo de archivo a crear
+const archivo = './db/data.json'
+
 //creamos nuestra funcion para guardar los datos en una txt
 const guardarDB = (data) => {
-    //en archivo especificamos el directorio y el tipo de archivo a crear
-    const archivo = './db/data.json'
     //mandamos el directorio y data a guardar
     fs.writeFileSync(archivo, JSON.stringify(data))
 }
 
+const leerDB = () => {
+
+    if (!fs.existsSync(archivo)) {
+        return null
+    }
+
+    const info = fs.readFileSync(archivo, { encoding: 'utf-8' })
+    const data = JSON.parse(info)
+    console.log(data)
+
+    return data
+}
+
+
 
 module.exports = {
-    guardarDB
+    guardarDB,
+    leerDB
 };
