1f0e06a398f18fd925112d58d74a2ab793388c71
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-10-23 23:59:12 -0400

    Se añadio listar tareas completadas y pendientes
    - ahora se muestran las tareas pendientes y completadas

--- 4.todo/models/tareas.js
+++ 4.todo/models/tareas.js
@@ -37,19 +37,47 @@ class Tareas {
         })
     }
 
-
+    //metodo para ver si una tarea esta completa o incompleta(completada:null)
     listadoCompleto() {
 
         console.log()
         this.listadoArr.forEach((tarea, i) => {
 
-            const idx = `${i + 1}`.green
+            const idx = `${i + 1}`.green        //cambiamos el index para que inicie en 1
             const { descripcion, completadoEn } = tarea
             const estado = (completadoEn)
                 ? 'Completada'.green
                 : 'Pendiente'.red
 
             console.log(`${idx}. ${descripcion}::${estado}`)
+        })
+    }
+
+    listarPendientesCompletadas(completadas = true) {
+
+        console.log()
+        let contador = 0
+        this.listadoArr.forEach(tarea => {
+
+            const { descripcion, completadoEn } = tarea
+            const estado = (completadoEn)
+                ? 'Completada'.green
+                : 'Pendiente'.red
+            if (completadas) {
+                //mostrar completadas
+                if (completadoEn) {
+                    contador += 1
+                    console.log(`${contador.toString().green}. ${descripcion}::${estado}`)
+
+                }
+            } else {
+                //mostrar tareas pendientes
+                if (!completadoEn) {
+                    contador += 1
+                    console.log(`${contador.toString().green}. ${descripcion}::${estado}`)
+
+                }
+            }
         })
     }
 }
